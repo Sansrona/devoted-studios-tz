2801ac637014d862ed5095f1482000da1b298c50
Sansrona <kingJaM00@yandex.ru>
2022-09-19 21:02:03 +0600

    init: start project

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,40 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div v-for="(key, item) in items" :key="item">{{item}} : {{key}}</div>
+
+  <div class="inputs">
+    <label for="text">название контента</label>
+    <input type="text" id="text" v-model="key" />
+    <label for="content">контент</label>
+    <input type="text" id="content" v-model="content" />
+    <button @click="submit">Submit</button>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import { ref, onMounted } from 'vue';
+const items = ref([]);
+const key = ref('');
+const content = ref('');
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+const submit = async () => {
+  const contents = content.value.split(',').map(i => i.trim());
+  let resContent = contents.length > 1 ? contents : contents[0];
+  let newItem = {
+    [key.value]: resContent,
   }
+  await fetch('http://localhost:8080/hash', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8'
+    },
+    body: JSON.stringify(newItem)
+  }).then(result => result.json()).then(result => items.value = { ...items.value, ...result });
 }
+
+onMounted(() => {
+  fetch('http://localhost:8080/hash').then(result => result.json()).then(result => items.value = result);
+});
 </script>
 
 <style>
@@ -22,5 +45,13 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.inputs {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding-top: 20px;
 }
 </style>
